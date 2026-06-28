8eefa7934bed1423ae04a32ffc80f1e1aab527fe
danielslima22 <danielslima22@gmail.com>
2026-06-28 18:16:18 -0300

    feat: Serilog + Seq configurados com logs estruturados

--- src/MedFlow.API/Program.cs
+++ src/MedFlow.API/Program.cs
@@ -2,47 +2,72 @@ using FluentValidation;
 using MedFlow.Agendamento.Domain.Entities;
 using MedFlow.Agendamento.Infrastructure;
 using MedFlow.Agendamento.Infrastructure.Repositories;
+using MedFlow.API.Middlewares;
 using MedFlow.SharedKernel.Behaviors;
 using MedFlow.SharedKernel.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// EF Core + PostgreSQL
-builder.Services.AddDbContext<AgendamentoDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
-
-// Repositorios
-builder.Services.AddScoped<IRepository<Consulta>, ConsultaRepository>();
-
-// UnitOfWork
-builder.Services.AddScoped<IUnitOfWork, AgendamentoUnitOfWork>();
-
-// MediatR + ValidationBehavior
-builder.Services.AddMediatR(cfg =>
+try
 {
-    cfg.RegisterServicesFromAssembly(
+    Log.Information("Iniciando MedFlow.API...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) =>
+        configuration.ReadFrom.Configuration(context.Configuration)
+                     .ReadFrom.Services(services)
+                     .Enrich.FromLogContext());
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    // EF Core + PostgreSQL
+    builder.Services.AddDbContext<AgendamentoDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
+
+    // Repositorios
+    builder.Services.AddScoped<IRepository<Consulta>, ConsultaRepository>();
+
+    // UnitOfWork
+    builder.Services.AddScoped<IUnitOfWork, AgendamentoUnitOfWork>();
+
+    // MediatR + ValidationBehavior
+    builder.Services.AddMediatR(cfg =>
+    {
+        cfg.RegisterServicesFromAssembly(
+            typeof(MedFlow.Agendamento.Application.Commands.AgendarConsultaCommand).Assembly);
+        cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    });
+
+    // FluentValidation
+    builder.Services.AddValidatorsFromAssembly(
         typeof(MedFlow.Agendamento.Application.Commands.AgendarConsultaCommand).Assembly);
-    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
-});
 
-// FluentValidation
-builder.Services.AddValidatorsFromAssembly(
-    typeof(MedFlow.Agendamento.Application.Commands.AgendarConsultaCommand).Assembly);
+    var app = builder.Build();
 
-var app = builder.Build();
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseSerilogRequestLogging();
 
-app.UseMiddleware<MedFlow.API.Middlewares.ErrorHandlingMiddleware>();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "MedFlow.API encerrou inesperadamente.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
